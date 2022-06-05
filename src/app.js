aae82cce786c0b3160dafb12299d2dc8713c2909
Wandeyajet <Wandeyajet@users.noreply.github.com>
2022-06-05 17:31:05 +0530

    created function for forecast with api for a 7 days weather

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ let day = days[date.getDay()];
 return  `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days =["Thu", "Fri", "Sat","Sun"];
@@ -34,7 +34,7 @@ function displayForecast() {
          <div class="weather-forecast-date" id="forecast">
          ${day}
      </div>
- <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+ <img src="https://openweathermap.org/img/wn/01d@2x.png" 
  alt=""
  width="35"
  />
@@ -46,11 +46,14 @@ function displayForecast() {
      `;
  });
 
-
-    forecastHTML=  forecastHTML +`</div>`;
-    
+forecastHTML=  forecastHTML +`</div>`;   
 forecastElement.innerHTML= forecastHTML;
+}
 
+function getForecast(coordinates){
+  let apiKey ="b24f5af8879849e3e5f4c2da8d700499"; 
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response){
@@ -76,6 +79,8 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000);
 iconElement.setAttribute("src", 
 `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
 }
 
 function search(city) {
